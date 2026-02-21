aa0967620424c20c0422d52b55c5c9047eabcc25
bhaveshburad729 <bhaveshburad729@gmail.com>
2026-02-21 23:16:54 +0530

    added sub domain director ecoomerse

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,6 +17,7 @@ const Navbar = () => {
                     </span>
                 </div>
                 <div className="hidden md:flex space-x-8 text-lg">
+                    <a href="/e-commerse/" className="hover:text-tronix-primary transition duration-300">Shop</a>
                     <a href="#internship" className="hover:text-tronix-primary transition duration-300">Internship</a>
                     <a href="#syllabus" className="hover:text-tronix-primary transition duration-300">Syllabus</a>
                     <a href="#about" className="hover:text-tronix-primary transition duration-300">About Us</a>
@@ -36,6 +37,7 @@ const Navbar = () => {
             {/* Mobile Menu */}
             {isMobileMenuOpen && (
                 <div id="mobile-menu" className="md:hidden bg-tronix-dark p-4 border-t border-tronix-primary/20">
+                    <a href="/e-commerse/" className="block py-2 text-center hover:text-tronix-primary transition duration-300" onClick={() => setIsMobileMenuOpen(false)}>Shop</a>
                     <a href="#internship" className="block py-2 text-center hover:text-tronix-primary transition duration-300" onClick={() => setIsMobileMenuOpen(false)}>Internship</a>
                     <a href="#syllabus" className="block py-2 text-center hover:text-tronix-primary transition duration-300" onClick={() => setIsMobileMenuOpen(false)}>Syllabus</a>
                     <a href="#about" className="block py-2 text-center hover:text-tronix-primary transition duration-300" onClick={() => setIsMobileMenuOpen(false)}>About Us</a>
